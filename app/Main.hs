d7a8a781ca57dd6cf2ff2f791050bdd6439fabcd
is junk <is4junk@hotmail.com>
2018-04-13 12:54:18 -0700

    Add cmdline options

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,24 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
 
 module Main where
 
 
 import Lib
+import qualified System.Console.CmdArgs as Args
 
+data MctsGames = MctsGames 
+    { gamesToPlay :: Int
+    , rollouts :: Int
+    }  deriving (Show, Args.Data, Args.Typeable)
+
+mctsGames :: MctsGames
+mctsGames = MctsGames 
+    { gamesToPlay = 10 Args.&= Args.help "number of concurrent games to play."
+    , rollouts    = 10 Args.&= Args.help "number of random game rollouts per move"
+    }
 
 main :: IO ()
-main = selfPlaysIO 100 50
+main = do 
+    (MctsGames gcnt rcnt )<- Args.cmdArgs mctsGames
+    selfPlaysIO gcnt rcnt
